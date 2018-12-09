5dba4ee73a7ae4517dcafaf26dceef9148c1881e
SaschaLK <SaschaL.Kufahl@gmail.com>
2018-12-09 04:25:43 -0800

    Slider cooldown + delay

---
+++ DIG/Assets/Scripts/SliderBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SliderBehaviour : MonoBehaviour, IBeginDragHandler, IEndDragHandler {
+
+    public float cooldownDelay;
+    public float cooldownRate;
+
+    private Slider slider;
+
+    private void Start() {
+        slider = GetComponent<Slider>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        StopAllCoroutines();
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        StartCoroutine(Cooldown());
+    }
+
+    IEnumerator Cooldown() {
+        yield return new WaitForSecondsRealtime(cooldownDelay);
+        float timeSlice = (slider.value / cooldownRate);
+        while (slider.value >= 0) {
+            slider.value -= timeSlice;
+            yield return new WaitForFixedUpdate();
+            if(slider.value <= 0) {
+                break;
+            }
+        }
+    }
+}
