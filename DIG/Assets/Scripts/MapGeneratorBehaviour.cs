ca0d0aee961a119221f9b59e85e1437fb378275e
SaschaLK <SaschaL.Kufahl@gmail.com>
2018-12-16 18:07:01 -0800

    cleaning and sorting of Map generator code base

--- DIG/Assets/Scripts/MapGeneratorBehaviour.cs
+++ DIG/Assets/Scripts/MapGeneratorBehaviour.cs
@@ -8,6 +8,7 @@ public class MapGeneratorBehaviour : MonoBehaviour {
 
     public Transform playerTransform;
 
+    //For generating background map
     public GameObject mapTile;
     public GameObject rowParent;
     public int mapRowTileAmountOdd;
@@ -25,6 +26,16 @@ public class MapGeneratorBehaviour : MonoBehaviour {
     private GameObject mapTilesRowThree;
     private int lowestPositionY = 0;
 
+    //For generating item noise images / maps
+    public Vector2Int oreDensity;
+    public int oreMapAmount;
+    [Range(0,1)]
+    public float spawnChance;
+    private List<bool[,]> oreMaps = new List<bool[,]>();
+    private Vector2 rowSize;
+    private Vector2 oreSpacing;
+
+    //Generating ore Type Lists and amount of ores;
     public int oreCacheAmount;
     public List<GameObject> oreTypes = new List<GameObject>();
     private List<List<GameObject>> oreLists = new List<List<GameObject>>();
@@ -34,18 +45,17 @@ public class MapGeneratorBehaviour : MonoBehaviour {
     }
 
     private void Start() {
+        //Generate Background Tiles, Parents and Lists of Background Tiles. Position them.
         GetSpriteSize();
         GenerateTileLists();
         PositionStartTiles();
-        GenerateOreLists();
-    }
 
-    //Method to reposition Maptile after it left vision of Camera
-    //Triggered by MapRow
-    public void PositionTileRow(Transform rowTransform) {
-        //TO-DO PLAYER CAN EXIT MAP IF HE GOES INTO ONE DIRECTION FOR LONG ENOUGH!
-        rowTransform.position = new Vector3(playerTransform.position.x, lowestPositionY * mapTileYSize, 0);
-        lowestPositionY--;
+        //Generate Ore Gameobjects and add to Lists / Noise Images
+        GenerateOreLists();
+        GenerateNoiseImagesForOreMaps();
+
+        //Position Ores for Start of Game via OreNoiseImages
+        PositionStartOres();
     }
 
     private void GetSpriteSize() {
@@ -107,6 +117,50 @@ public class MapGeneratorBehaviour : MonoBehaviour {
         }
     }
 
+    //Foreach LoadScene and PlayGame multiple image maps are generated via noise images
+    private void GenerateNoiseImagesForOreMaps() {
+        //Getting RowSize
+        rowSize = new Vector2(mapTileParents[0].GetComponent<BoxCollider2D>().size.x, mapTileParents[0].GetComponent<BoxCollider2D>().size.y);
+        oreSpacing = new Vector2(rowSize.x / oreDensity.x, rowSize.y / oreDensity.y);
+
+        //Creating noise images for ore maps / creating ore maps
+        for(int a = 0; a < oreMapAmount; a++) {
+            bool[,] oreMap = new bool[oreDensity.x, oreDensity.y];
+            for(int i = 0; i < oreDensity.x; i++) {
+                for(int k = 0; k < oreDensity.y; k++) {
+                    if(Random.Range(0f, 1f) < spawnChance) {
+                        oreMap[i, k] = true;
+                    }
+                }
+            }
+            oreMaps.Add(oreMap);
+        }
+    }
+
+    //TO DO, EACH ROW OF MAP HAS TO HAVE IT'S OWN ORES AND ORE NOISE MAP
+    //Place one image on each row of map; Fill each true node with one ore
+    private void PositionStartOres() {
+        int tempOreIndex = 0;
+        foreach(GameObject mapRow in mapTileParents) {
+            Vector2 startNode = new Vector2(-rowSize.x / 2, mapRow.transform.position.y + (rowSize.y / 2));
+            for(int i = 0; i < oreDensity.x; i++) {
+                for(int k = 0; k < oreDensity.y; k++) {
+                    oreLists[0][tempOreIndex].transform.position = new Vector3(startNode.x + (k * oreSpacing.x), startNode.y - (i * oreSpacing.y), oreLists[0][tempOreIndex].transform.position.z);
+                    tempOreIndex++;
+                }
+            }
+        }
+    }
+
+    //Method to reposition Maptile after it left vision of Camera
+    //Triggered by MapRow
+    public void PositionTileRow(Transform rowTransform) {
+        //TO-DO PLAYER CAN EXIT MAP IF HE GOES INTO ONE DIRECTION FOR LONG ENOUGH!
+        rowTransform.position = new Vector3(playerTransform.position.x, lowestPositionY * mapTileYSize, 0);
+        lowestPositionY--;
+    }
+
+    //Position new ore row with random image from open selection and place ores in new row on trigger exit of old row
     public void PositionOres(Transform rowTransform) {
         Debug.Log("position");
     }
