b56d298350521b7481bbca48c08a8d5a9ba1eb09
SaschaLK <SaschaL.Kufahl@gmail.com>
2020-02-19 01:57:48 +0100

    BG rework

---
+++ DIG/Assets/Scripts/BgGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgGenerator : MonoBehaviour {
+
+    public GameObject bgRow;
+
+    private void Start() {
+        for(int i = -14; i < 15; i++) {
+            Instantiate(bgRow, new Vector3(0, i, 0), Quaternion.identity);
+        }
+    }
+}
---
+++ DIG/Assets/Scripts/BgRowBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgRowBehaviour : MonoBehaviour {
+    private void OnTriggerExit2D(Collider2D collision) {
+        transform.position = new Vector3(collision.transform.position.x, collision.transform.position.y - 14, 0);
+    }
+
+
+
+
+
+}
