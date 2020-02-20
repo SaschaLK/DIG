f669856fd890d7ad5d63fc51c249417a0b41c05d
SaschaLK <SaschaL.Kufahl@gmail.com>
2020-02-20 20:22:21 +0100

    Fix map row repositioning

--- DIG/Assets/Scripts/BgRowBehaviour.cs
+++ DIG/Assets/Scripts/BgRowBehaviour.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class BgRowBehaviour : MonoBehaviour {
     private void OnTriggerExit2D(Collider2D collision) {
-        transform.position = new Vector3(collision.transform.position.x, collision.transform.position.y - 14, 0);
+        transform.position = new Vector3(collision.transform.position.x, transform.position.y - 29, 0);
     }
-
-
-
-
-
 }
